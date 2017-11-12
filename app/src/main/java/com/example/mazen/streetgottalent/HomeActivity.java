898cd083bb709d47223c40a4930eff96cd850f68
Mazen <mazen.denden@esprit.tn>
2017-11-12 16:53:12 +0100

    Login Task

--- app/src/main/java/com/example/mazen/streetgottalent/HomeActivity.java
+++ app/src/main/java/com/example/mazen/streetgottalent/HomeActivity.java
@@ -1,18 +1,61 @@
 package com.example.mazen.streetgottalent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    TextView helloText;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    HomeFragment frag = new HomeFragment();
+                    android.app.FragmentManager fragmentManager = getFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.content, frag)
+                            .commit();
+                    return true;
+                case R.id.navigation_dashboard:
+
+                    return true;
+                case R.id.navigation_profile:
+                    ProfileFragment frag2 = new ProfileFragment();
+                    android.app.FragmentManager fragmentManager2 = getFragmentManager();
+                    fragmentManager2.beginTransaction()
+                            .replace(R.id.content, frag2)
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        helloText = (TextView) findViewById(R.id.helloText);
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        HomeFragment frag = new HomeFragment();
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content, frag)
+                .commit();
 
     }
+
 }
